9427d4890d89baa6da6bdc37116c1b5ee8ccb53b
smmartprogrammer <partabrajp@gmail.com>
2023-07-21 23:49:46 +0500

    store

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import cartSlice from './slice/cartSlice'
-
+import cartSlice from './cartSlice'
 
 export const store = configureStore({
   reducer: {
